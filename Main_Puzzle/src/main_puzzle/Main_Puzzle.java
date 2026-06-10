4e1e0e66dfdbaac0d86fe019a076d5d206981b62
Suarez_VV <vladyasuave@gmail.com>
2026-06-10 11:03:01 -0600

    Parte de GUI-Visual y movimiento de fichas

--- Main_Puzzle/src/main_puzzle/Main_Puzzle.java
+++ Main_Puzzle/src/main_puzzle/Main_Puzzle.java
@@ -3,6 +3,12 @@ package main_puzzle;
 public class Main_Puzzle {
 
     public static void main(String[] args) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GraphicPuzzle().setVisible(true);
+            }
+        });
     }
     
 }
